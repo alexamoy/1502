d5c50b66f1323a3460d444a2a21ffa05aa19c254
Alexa Moy <s.alexa.moy@gmail.com>
2019-07-23 15:50:39 -0400

    dynamic shopping list

--- client/components/ShoppingList.js
+++ client/components/ShoppingList.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import { ListGroup, Form } from 'react-bootstrap';
 
 export default class ShoppingList extends React.Component {
   constructor(){
     super();
+    this.state = {
+      list: [
+        {name: 'Item 1'},
+        {name: 'Item 2'},
+        {name: 'Item 3'}
+      ]
+    };
   }
 
   render() {
     return (
-      <div id="shopping-list-container">
-        <h1>Pat is a butthole</h1>
+      <div id="shopping-list-container" className="vw-100">
+        <ListGroup className="w-75">
+          {
+            this.state.list.map((item, key) => {
+              return (
+                <ListGroup.Item key={key}>
+                  <div className="d-flex flex-row justify-content-between">
+                    <span>{item.name}</span>
+                    <Form.Check />
+                  </div>
+                </ListGroup.Item>
+              );
+            })
+          }
+        </ListGroup>
       </div>
     );
   }
